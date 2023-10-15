b5d16c0bf7c5eb5898a8c8cf262d1ec318584d3a
Sanchayan Paramsothy <Sanchayan.Paramsothy1@ibm.com>
2023-10-15 18:29:13 +0800

    book init

---
+++ src/test/java/com/io/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
